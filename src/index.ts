ffa7e5e7b7a916678592ada1d8096e6f9aa21b85
Grantopher <shadowcarson@gmail.com>
2019-01-30 22:55:39 -0800

    Expressing [WIP]

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,15 @@
-import {Db, MongoClient, MongoError} from 'mongodb';
-import { Express } from 'express-serve-static-core';
-import { ICharacter } from "./Interfaces";
+import express from "express";
 
-const url: string = "mongodb://localhost:27017";
+const app = express();
+const port = 8080; // default port to listen
 
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
 
-MongoClient.connect(url, async (error: MongoError, client: MongoClient) => {
-    if (error) throw error;
-    const db: Db = client.db('character-sheets');
-    const char: ICharacter = genCharacter();
-    await db.collection('Sheets').insertOne(char);
-    return client.close();
-});
-
-function genCharacter(): ICharacter {
-    return {
-        name: "STEVE",
-        race: "Dwarf",
-        class: [{ name: 'cleric', level: 1}],
-        experience: 300,
-        background: 'wanderer',
-        player_name: 'Derek',
-        hit_points: 42,
-        stats: {
-            strength: 10,
-            dexterity: 10,
-            constitution: 10,
-            intelligence: 10,
-            wisdom: 10,
-            charisma: 10
-        }
-    }
-}
+// start the Express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.info( `server started at http://localhost:${ port }` );
+} );
