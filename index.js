965eecd7db562859e006a3806d43d07a0291795b
Grantopher <shadowcarson@gmail.com>
2019-01-30 21:02:41 -0800

    Messing with inserting documents

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = "mongodb://localhost:27017/character-sheets";
+const url = "mongodb://localhost:27017";
 
-MongoClient.connect(url, (error, db) => {
-    if (error) {
-        console.error(error);
-        throw error;
-    }
-    console.info("Database created");
-    db.close();
+MongoClient.connect(url, (error, client) => {
+    if (error) throw error;
+    const db = client.db('character-sheets');
+    const char = genCharacter();
+    db.collection('Sheets').insertOne(char);
+    client.close();
 });
+
+
+function genCharacter() {
+    return {
+        name: "STEVE",
+        race: "Dwarf"
+    }
+}
