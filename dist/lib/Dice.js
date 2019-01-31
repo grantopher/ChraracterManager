e04ccf062bd9b1e4e376dcd05324bce8f8f52c0d
Grantopher <shadowcarson@gmail.com>
2019-01-31 01:31:16 -0800

    ignores dist

--- dist/lib/Dice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Dice {
-    static D(sides) {
-        return Math.ceil(Math.random() * sides);
-    }
-}
-exports.Dice = Dice;
-//# sourceMappingURL=Dice.js.map
